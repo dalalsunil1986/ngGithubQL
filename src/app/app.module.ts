54c8769218bacea2b2f030013f4adf80e496f9ab
ikismail <ikismail7@gmail.com>
2018-05-25 05:39:28 +0530

    Created Basic Project and Implemented bootstrap template for application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TopNavComponent,
+    DashboardComponent
+],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/top-nav/top-nav.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "git-top-nav",
+  templateUrl: "./top-nav.component.html",
+  styleUrls: ["./top-nav.component.css"]
+})
+export class TopNavComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
